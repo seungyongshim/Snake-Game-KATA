83810831a7a4191d2061418b7e864f4caafc55c8
심승용 <seungyong.shim@mirero.co.kr>
2020-09-22 08:11:54 +0900

    우측 방향 이동 테스트 작성

---
+++ Snake.Tests/SnakeSpec.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Snake.Tests
+{
+    public class SnakeSpec
+    {
+        [Fact]
+        public void Should_Be_Right_Forward_Move()
+        {
+            // arrange
+            var snake = new Snake(5, 5);
+
+            // act
+            snake.Move();
+
+            // assert
+            snake.Body.ToArray().Should().BeEquivalentTo(new[] { (5, 6) });
+        }
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public enum Direction
+    {
+        Left, Right, Up, Down
+    }
+
+    public class Snake
+    {
+        public Snake(int initY, int initX)
+        {
+            Body.Enqueue((initY, initX));
+        }
+
+        public Queue<(int, int)> Body { get; set; } = new Queue<(int, int)>();
+
+        public Direction Head { get; set; } = Direction.Right;
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
