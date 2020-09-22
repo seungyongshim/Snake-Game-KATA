4803a7d821ce904e357312a2861f1ee02c6cac0a
심승용 <seungyong.shim@mirero.co.kr>
2020-09-22 09:29:01 +0900

    Map 생성 테스트 작성

---
+++ Snake.Tests/MapSpec.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Snake.Tests
+{
+    public class MapSpec
+    {
+        [Fact]
+        public void Should_Be_Make_Map()
+        {
+            // arrange 
+            var map = new Map(5, 5);
+
+            // act
+
+
+            // assert
+            map.ToString().Should().Be(". . . . . \n" +
+                                       ". . . . . \n" +
+                                       ". . . . . \n" +
+                                       ". . . . . \n" +
+                                       ". . . . . \n");
+        }
+    }
+}
---
+++ Snake/Map.cs
@@ -0,0 +1,18 @@
+﻿namespace Snake
+{
+    public class Map
+    {
+        public Map(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height { get; }
+        public int Width { get; }
+
+        private class Item
+        {
+        }
+    }
+}
