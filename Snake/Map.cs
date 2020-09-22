083f68bcd50e0258650ceb44d12c5f928bf956d9
심승용 <seungyong.shim@mirero.co.kr>
2020-09-22 10:54:58 +0900

    뱀 생성 구현

--- Snake/Map.cs
+++ Snake/Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Snake
 {
@@ -14,8 +15,8 @@ namespace Snake
 
         public int Height { get; }
         public int Width { get; }
-        
-        
+        public Snake Snake { get; private set; }
+
         public override string ToString()
         {
             var Items = new Item[Height][];
@@ -25,16 +26,37 @@ namespace Snake
                 Items[i] = new Item[Width];
             }
 
+            if (Snake != null)
+            {
+                foreach (var (y, x) in Snake.Body)
+                {
+                    Items[y][x].SetSnake();
+                }
+            }
+
             return Items.Select(y => y.Aggregate(Empty, (c, i) => c + i + " "))
                         .Aggregate(Empty, (c, i) => c + i + "\n");
         }
 
         public struct Item
         {
+            public bool IsSnake { get; private set; }
+
             public override string ToString()
             {
-                return ".";
+                return IsSnake? "O": ".";
             }
+
+            public void SetSnake()
+            {
+                IsSnake = true;
+            }
+        }
+
+        public void GenerateSnake()
+        {
+            Snake = new Snake(Math.Abs(Width / 2), Math.Abs(Height / 2));
+            Snake.Move(Apple.Exist);
         }
     }
 }
