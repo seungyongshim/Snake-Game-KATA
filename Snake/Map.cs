2662e3294892ad7a3ba9f96f0c886500044e683d
심승용 <seungyong.shim@mirero.co.kr>
2020-09-22 09:59:00 +0900

    Map 생성 구현

--- Snake/Map.cs
+++ Snake/Map.cs
@@ -1,5 +1,9 @@
-﻿namespace Snake
+﻿using System.Linq;
+
+namespace Snake
 {
+    using static System.String;
+
     public class Map
     {
         public Map(int height, int width)
@@ -10,9 +14,27 @@
 
         public int Height { get; }
         public int Width { get; }
-
-        private class Item
+        
+        
+        public override string ToString()
         {
+            var Items = new Item[Height][];
+
+            for (var i = 0; i < Height; i++)
+            {
+                Items[i] = new Item[Width];
+            }
+
+            return Items.Select(y => y.Aggregate(Empty, (c, i) => c + i + " "))
+                        .Aggregate(Empty, (c, i) => c + i + "\n");
+        }
+
+        public struct Item
+        {
+            public override string ToString()
+            {
+                return ".";
+            }
         }
     }
 }
