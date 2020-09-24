96abb0e0985dd77125afec7affc2e2115e2e7c10
심승용 <seungyong.shim@mirero.co.kr>
2020-09-24 10:44:15 +0900

    Rx.NET 적용

---
+++ README.md
@@ -0,0 +1,8 @@
+# Snake-Game-KATA
+## 목표
+- TDD로 스네이크 게임을 작성한다.
+## 콘솔 단계
+- [x] console 기반 tdd 구현
+- [ ] Rx.NET을 적용한 리펙토링
+- [ ] LanguageExt를 적용한 함수형 리펙토링
+
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace Snake
 {
     class Program
     {
+        // https://stackoverflow.com/questions/10675451/iobservable-of-keys-pressed
+        static IEnumerable<ConsoleKeyInfo> KeyPresses()
+        {
+            do
+            {
+                var key = Console.ReadKey();
+                yield return key;
+            } while (true);
+        }
+
         static void Main(string[] args)
         {
             var map = new Map(25, 25);
@@ -34,21 +46,23 @@ namespace Snake
                 Console.WriteLine("GameOver");
             });
 
-            while (true)
-            {
-                var key = Console.ReadKey();
+            var keys = KeyPresses().ToObservable();
 
-                var direction = key.Key switch
+            keys.Throttle(TimeSpan.FromMilliseconds(66))
+                .Subscribe(key =>
                 {
-                    ConsoleKey.UpArrow => Direction.Up,
-                    ConsoleKey.DownArrow => Direction.Down,
-                    ConsoleKey.LeftArrow => Direction.Left,
-                    ConsoleKey.RightArrow => Direction.Right,
-                    _ => map.Snake.Direction,
-                };
+                    var direction = key.Key switch
+                    {
+                        ConsoleKey.UpArrow => Direction.Up,
+                        ConsoleKey.DownArrow => Direction.Down,
+                        ConsoleKey.LeftArrow => Direction.Left,
+                        ConsoleKey.RightArrow => Direction.Right,
+                        _ => map.Snake.Direction,
+                    };
 
-                map.Snake.SetDirection(direction);
-            }
+                    map.Snake.SetDirection(direction);
+                });
         }
     }
 }
+
